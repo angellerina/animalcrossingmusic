ee4569c46fb0909f0010fb949d74d52f73dbeaa9
angellerina <angellerinac@gmail.com>
2021-02-06 23:21:47 -0500

    customized input range

--- src/App.js
+++ src/App.js
@@ -24,7 +24,17 @@ function App(prop) {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
+    //Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const percentage = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration: duration,
+      animationPercentage: percentage,
+      volume: e.target.volume,
+    });
   };
 
   let image;
--- src/components/Player.js
+++ src/components/Player.js
@@ -58,18 +58,25 @@ const Player = ({
     }
     playAudio(isPlaying, audioRef);
   };
+  //Add the styles
+  const trackAnim = {
+    transform: `translateX(${songInfo.animationPercentage}%)`,
+  };
 
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input
-          min={0}
-          max={songInfo.duration || "0"}
-          value={songInfo.currentTime}
-          onChange={dragHandler}
-          type="range"
-        />
+        <div className="track">
+          <input
+            min={0}
+            max={songInfo.duration || "0"}
+            value={songInfo.currentTime}
+            onChange={dragHandler}
+            type="range"
+          />
+          <div style={trackAnim} className="animate-track"></div>
+        </div>
         <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
       </div>
       <div className="play-control">
